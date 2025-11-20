37e4298f43d85b589251c868391e60aa3090eb82
Toma Rosa <toma.rosa@icloud.com>
2025-11-20 10:38:58 +0100

    chore: add tailwind css and prettier
    Signed-off-by: Toma Rosa <toma.rosa@icloud.com>

---
+++ astro.config.ts
@@ -0,0 +1,24 @@
+// @ts-check
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig, fontProviders } from "astro/config";
+
+// https://astro.build/config
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  experimental: {
+    fonts: [
+      {
+        provider: fontProviders.fontsource(),
+        name: "Montserrat",
+        cssVariable: "--font-montserrat",
+      },
+      {
+        provider: fontProviders.fontsource(),
+        name: "Inter",
+        cssVariable: "--font-inter",
+      },
+    ],
+  },
+});
